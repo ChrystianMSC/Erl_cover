2e9ce0eaece51cf6a6d0ce48219bfd6ca6359dce
chrystianmsc <chrystianmsc@gmail.com>
2022-11-27 13:34:32 -0300

    comments

--- parser.erl
+++ parser.erl
@@ -3,24 +3,28 @@
 -compile(export_all).
 
 evaluate_expression() ->
-    % defining couter and adding it to persistent term
+    % Reading and parsing the code
     {ok, File} = file:read_file("covered.erl"),
     Expression = unicode:characters_to_list(File),
     {ok, Tokens, _} = erl_scan:string(Expression),
     {ok, Parsed} = erl_parse:parse_exprs(Tokens),
 
+    % Create a counter where each position is a branch
     SizeP = length(Parsed),
     Counter = counters:new(SizeP, [atomics]),
+    % Add the ccounter to the persistent_term
     persistent_term:put(1, Counter),
 
+    % Instrumenting the code
     Parsedin = [],
     ParsedinF = parsed_in(Parsed, SizeP, 1, Parsedin),
 
+    % Execute the code and see the result
     {value, Result, _} = erl_eval:exprs(ParsedinF, []),
     get_result(SizeP, Parsed, 1),
     Result.
 
-
+% Instrument each branch individually, recursively
 parsed_in(Parsed, SizeP, Now, Parsedin) ->
     if
         Now =< SizeP ->
@@ -31,25 +35,27 @@ parsed_in(Parsed, SizeP, Now, Parsedin) ->
     end.
 
 
-
+% Use the result of the instrumentantion and print the clauses that where executed in each branch
 get_result(SizeP, Parsed, Now) ->
     if
         Now =< SizeP ->
-            Tipe = tuple_to_list(lists:nth(Now, Parsed)),
-            Cond = lists:nth(1, Tipe),
-            Line = lists:nth(2, Tipe),
+            Branch = tuple_to_list(lists:nth(Now, Parsed)),
+            Cond = lists:nth(1, Branch),
+            Line = lists:nth(2, Branch),
             if 
                 Cond == 'if' ->
+                    % Print the branch name in this case the 'if' and its line
                     io:format("~p/~p -> ", [Cond, Line]),
-                    ClauseList = lists:nth(3, Tipe),
-                    % Getting the counter for printing if
+                    ClauseList = lists:nth(3, Branch),
+                    % Print the branch clauses differentiating what was and what was not executed
                     print_clauses_if(ClauseList, counters:get(persistent_term:get(1),Now), length(ClauseList)+1, 1),
                     io:fwrite("~n~n"),
                     get_result(SizeP, Parsed, Now + 1);
                 Cond == 'case' ->
+                    % Print the branch name in this case the 'case' and its line
                     io:format("~p/~p -> ", [Cond, Line]),
-                    ClauseList = lists:nth(4, Tipe),
-                    % Getting the counter for printing case (maybe not working)
+                    ClauseList = lists:nth(4, Branch),
+                    % Print the branch clauses differentiating what was and what was not executed
                     print_clauses_case(ClauseList, counters:get(persistent_term:get(1),Now), length(ClauseList)+1, 1),
                     io:fwrite("~n~n"),
                     get_result(SizeP, Parsed, Now + 1);
@@ -60,6 +66,9 @@ get_result(SizeP, Parsed, Now) ->
             ok
     end.
 
+% ------------------------------------------ PRINT CLAUSES------------------------------------------------------------
+
+% For each clause of the if it prints if it was or not executed
 print_clauses_if(Clauses, Num, Length, Current) ->
     if
         Current == Num ->
@@ -93,9 +102,11 @@ print_clauses_case(Clauses, Num, Length, Current) ->
             io:fwrite("Clause not executed/~p -> ", [Line]),
             print_clauses_case(Clauses, Num, Length, Current + 1)
     end.
+% ------------------------------------------ PRINT CLAUSES/ ------------------------------------------------------------
 
-% ------------------------------------- Automatic Instrumentation ------------------------------------ %
+% ------------------------------------- Automatic Instrumentation ------------------------------------------------------
 
+% Returns the Branch with its clauses insturmented
 instrumenting(Parsed, Now) ->
     Tipe = tuple_to_list(lists:nth(Now, Parsed)),
     Cond = lists:nth(1, Tipe),
