192b4a7c1934f3e7bcb0f35416fd619af429517d
chrystianmsc <chrystianmsc@gmail.com>
2022-11-27 12:39:02 -0300

    consecutive branches

--- covered.erl
+++ covered.erl
@@ -1,1 +1,3 @@
-if 1 == 1 -> true; true -> false end.
+if 1 == 1 -> if 1 == 1 -> true; true -> false end; true -> false end,
+if 1 == 1 -> if 1 == 1 -> true; true -> false end; true -> false end,
+case 5 of  5 -> true; 6 -> false end.
--- parser.erl
+++ parser.erl
@@ -2,71 +2,6 @@
 
 -compile(export_all).
 
-parse_nested(Parsed) ->
-    SizeP = length(Parsed),
-    if
-        SizeP == 1 ->
-            Counter = counters:new(1, [atomics]),
-            persistent_term:put(1, Counter),
-            Parsedin = [instrumenting(Parsed)],
-            {value, Result, _} = erl_eval:exprs(Parsedin, []),
-            Tipe = tuple_to_list(lists:nth(1, Parsed)),
-            Cond = lists:nth(1, Tipe),
-            Line = lists:nth(2, Tipe),
-            if 
-                Cond == 'if' ->
-                    io:format("~p/~p -> ", [Cond, Line]),
-                    ClauseList = lists:nth(3, Tipe),
-                    % Getting the counter for printing if
-                    print_clauses_if(ClauseList, counters:get(persistent_term:get(1),1), length(ClauseList)+1, 1),
-                    io:fwrite("~n~n");
-                Cond == 'case' ->
-                    io:format("~p/~p -> ", [Cond, Line]),
-                    ClauseList = lists:nth(4, Tipe),
-                    % Getting the counter for printing case (maybe not working)
-                    print_clauses_case(ClauseList, counters:get(persistent_term:get(1),1), length(ClauseList)+1, 1),
-                    io:fwrite("~n~n");
-                true ->
-                    io:fwrite("Nada ~n")
-            end,
-            io:format("           ~p~n~n", [Result]);
-        true ->
-            multi_branch(Parsed, SizeP, 1)
-    end.
-
-
-multi_branch(Parsed, SizeP, BranchN) ->
-    if
-        BranchN > SizeP ->
-            ok;
-        true ->
-            Counter = counters:new(1, [atomics]),
-            persistent_term:put(1, Counter),
-            Parsedin = [instrumenting(Parsed)],
-            {value, Result, _} = erl_eval:exprs(Parsedin, []),
-            Tipe = tuple_to_list(lists:nth(BranchN, Parsed)),
-            Cond = lists:nth(1, Tipe),
-            Line = lists:nth(2, Tipe),
-            if 
-                Cond == 'if' ->
-                    io:format("~p/~p -> ", [Cond, Line]),
-                    ClauseList = lists:nth(3, Tipe),
-                    % Getting the counter for printing if
-                    print_clauses_if(ClauseList, counters:get(persistent_term:get(1),1), length(ClauseList)+1, 1),
-                    io:fwrite("~n~n");
-                Cond == 'case' ->
-                    io:format("~p/~p -> ", [Cond, Line]),
-                    ClauseList = lists:nth(4, Tipe),
-                    % Getting the counter for printing case (maybe not working)
-                    print_clauses_case(ClauseList, counters:get(persistent_term:get(1),1), length(ClauseList)+1, 1),
-                    io:fwrite("~n~n");
-                true ->
-                    io:fwrite("Nada ~n")
-            end,
-            io:format("~p", [Result]),
-            multi_branch(Parsed, SizeP, BranchN + 1)
-    end.
-
 evaluate_expression() ->
     % defining couter and adding it to persistent term
     {ok, File} = file:read_file("covered.erl"),
@@ -75,14 +10,31 @@ evaluate_expression() ->
     {ok, Parsed} = erl_parse:parse_exprs(Tokens),
 
     SizeP = length(Parsed),
+    Counter = counters:new(SizeP, [atomics]),
+    persistent_term:put(1, Counter),
 
+    Parsedin = [],
+    ParsedinF = parsed_in(Parsed, SizeP, 1, Parsedin),
+
+    {value, Result, _} = erl_eval:exprs(ParsedinF, []),
+    get_result(SizeP, Parsed, 1),
+    Result.
+
+
+parsed_in(Parsed, SizeP, Now, Parsedin) ->
     if
-        SizeP == 1 ->
-            Counter = counters:new(1, [atomics]),
-            persistent_term:put(1, Counter),
-            Parsedin = [instrumenting(Parsed)],
-            {value, Result, _} = erl_eval:exprs(Parsedin, []),
-            Tipe = tuple_to_list(lists:nth(1, Parsed)),
+        Now =< SizeP ->
+            ParsedinF = Parsedin ++ [instrumenting(Parsed, Now)],
+            parsed_in(Parsed, SizeP, Now+1, ParsedinF);
+        true ->
+            Parsedin
+    end.
+
+
+get_result(SizeP, Parsed, Now) ->
+    if
+        Now =< SizeP ->
+            Tipe = tuple_to_list(lists:nth(Now, Parsed)),
             Cond = lists:nth(1, Tipe),
             Line = lists:nth(2, Tipe),
             if 
@@ -90,21 +42,23 @@ evaluate_expression() ->
                     io:format("~p/~p -> ", [Cond, Line]),
                     ClauseList = lists:nth(3, Tipe),
                     % Getting the counter for printing if
-                    print_clauses_if(ClauseList, counters:get(persistent_term:get(1),1), length(ClauseList)+1, 1),
-                    io:fwrite("~n~n");
+                    print_clauses_if(ClauseList, counters:get(persistent_term:get(1),Now), length(ClauseList)+1, 1),
+                    io:fwrite("~n~n"),
+                    get_result(SizeP, Parsed, Now + 1);
                 Cond == 'case' ->
                     io:format("~p/~p -> ", [Cond, Line]),
                     ClauseList = lists:nth(4, Tipe),
                     % Getting the counter for printing case (maybe not working)
-                    print_clauses_case(ClauseList, counters:get(persistent_term:get(1),1), length(ClauseList)+1, 1),
-                    io:fwrite("~n~n");
+                    print_clauses_case(ClauseList, counters:get(persistent_term:get(1),Now), length(ClauseList)+1, 1),
+                    io:fwrite("~n~n"),
+                    get_result(SizeP, Parsed, Now + 1);
                 true ->
                     io:fwrite("Nada ~n")
-            end,
-            Result;
+            end;
         true ->
-            multi_branch(Parsed, SizeP, 1)
+            ok
     end.
+
 
 print_clauses_if(Clauses, Num, Length, Current) ->
     if
@@ -113,19 +67,6 @@ print_clauses_if(Clauses, Num, Length, Current) ->
             Opr = lists:nth(4, Expr),
             Line = lists:nth(2, Expr),
             io:format("~p/~p -> ", [Opr, Line]),
-            Inside = lists:nth(5, Expr),
-            Tipe = tuple_to_list(lists:nth(1, Inside)),
-            Cond = lists:nth(1, Tipe),
-            if
-                Cond == 'if' ->
-                    io:format("~n~n          "), 
-                    parse_nested(Inside);
-                Cond == 'case' ->
-                    io:format("~n~n          "), 
-                    parse_nested(Inside);
-                true ->
-                    ok
-            end,
             print_clauses_if(Clauses, Num, Length, Current + 1);
         Current == Length ->
             ok;
@@ -143,19 +84,6 @@ print_clauses_case(Clauses, Num, Length, Current) ->
             Opr = lists:nth(3, Expr),
             Line = lists:nth(2, Expr),
             io:format("~p/~p -> ", [Opr, Line]),
-            Inside = lists:nth(5, Expr),
-            Tipe = tuple_to_list(lists:nth(1, Inside)),
-            Cond = lists:nth(1, Tipe),
-            if
-                Cond == 'if' ->
-                    io:format("~n~n          "), 
-                    parse_nested(Inside);
-                Cond == 'case' ->
-                    io:format("~n~n          "), 
-                    parse_nested(Inside);
-                true ->
-                    ok
-            end,
             print_clauses_case(Clauses, Num, Length, Current + 1);
         Current == Length ->
             ok;
@@ -168,38 +96,38 @@ print_clauses_case(Clauses, Num, Length, Current) ->
 
 % ----------------------- Automatic Instrumentation ------------------------------ %
 
-instrumenting(Parsed) ->
-    Tipe = tuple_to_list(lists:nth(1, Parsed)),
+instrumenting(Parsed, Now) ->
+    Tipe = tuple_to_list(lists:nth(Now, Parsed)),
     Cond = lists:nth(1, Tipe),
     if 
         Cond == 'if' ->
             ClauseList = lists:nth(3, Tipe),
-            Clauselistin = instrument_clauses(ClauseList, length(ClauseList)+1, 1),
+            Clauselistin = instrument_clauses(ClauseList, length(ClauseList)+1, 1, Now),
             Clauselistin2 = erlang:delete_element(length(Clauselistin), list_to_tuple(Clauselistin)),
             Clauselistin3 = tuple_to_list(Clauselistin2),
-            Parsedin = setelement(3, lists:nth(1, Parsed), Clauselistin3),
+            Parsedin = setelement(3, lists:nth(Now, Parsed), Clauselistin3),
             Parsedin;
         Cond == 'case' ->
             ClauseList = lists:nth(4, Tipe),
-            Clauselistin = instrument_clauses(ClauseList, length(ClauseList)+1, 1),
+            Clauselistin = instrument_clauses(ClauseList, length(ClauseList)+1, 1, Now),
             Clauselistin2 = erlang:delete_element(length(Clauselistin), list_to_tuple(Clauselistin)),
             Clauselistin3 = tuple_to_list(Clauselistin2),
-            Parsedin = setelement(4, lists:nth(1, Parsed), Clauselistin3),
+            Parsedin = setelement(4, lists:nth(Now, Parsed), Clauselistin3),
             Parsedin;
         true ->
             io:fwrite("Nada ~n")
     end.
 
-instrument_clauses(ClauseList, Length, Current) ->
+instrument_clauses(ClauseList, Length, Current, Now) ->
     if
         Current < Length ->
             Clause = tuple_to_list(lists:nth(Current, ClauseList)),
             Line = lists:nth(2, Clause),
             % adding to target code the AST of a code that adds the clause number to the counter
-            Instrumentation = [{call,Line,{remote,Line,{atom,Line,counters},{atom,Line,add}},[{call,Line,{remote,Line,{atom,Line,persistent_term},{atom,Line,get}},[{integer,Line,1}]},{integer,Line,1},{integer,Line,Current}]}],
+            Instrumentation = [{call,Line,{remote,Line,{atom,Line,counters},{atom,Line,add}},[{call,Line,{remote,Line,{atom,Line,persistent_term},{atom,Line,get}},[{integer,Line,1}]},{integer,Line,Now},{integer,Line,Current}]}],
             Item = Instrumentation ++ lists:nth(5, Clause),
             Clausein = setelement(5, lists:nth(Current, ClauseList), Item),
-            Clauselistin = [Clausein] ++ instrument_clauses(ClauseList, Length, Current + 1),
+            Clauselistin = [Clausein] ++ instrument_clauses(ClauseList, Length, Current + 1, Now),
             Clauselistin;
         true ->
             Fim = [ok],
