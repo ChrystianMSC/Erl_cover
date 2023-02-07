7761bc6023ffe2e39bfec477937b6baf0234ea34
chrystianmsc <chrystianmsc@gmail.com>
2023-02-07 19:24:14 -0300

    commit

--- test/covered.erl
+++ test/covered.erl
@@ -1,4 +1,6 @@
 A = 1,
+B = 2,
+C = 3, 
 if 1 == 1 -> if 1 == 1 -> true; true -> false end; true -> false end,
 if 1 == 1 -> if 1 == 1 -> true; true -> false end; true -> false end,
 case 5 of  5 -> true; 6 -> false end.
